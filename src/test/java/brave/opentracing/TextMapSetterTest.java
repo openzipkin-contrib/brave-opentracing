bd96addc485a5983b241f262e41bcd0d8d3d00f4
Adrian Cole <acole@pivotal.io>
2018-03-04 09:26:12 +0800

    Updates to latest brave

--- src/test/java/brave/opentracing/TextMapSetterTest.java
+++ src/test/java/brave/opentracing/TextMapSetterTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016-2017 The OpenZipkin Authors
+ * Copyright 2016-2018 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -14,7 +14,7 @@
 package brave.opentracing;
 
 import brave.propagation.Propagation;
-import brave.propagation.PropagationSetterTest;
+import brave.test.propagation.PropagationSetterTest;
 import io.opentracing.propagation.TextMap;
 import io.opentracing.propagation.TextMapInjectAdapter;
 import java.util.Collections;
