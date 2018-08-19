eb5420f6f80accfac2e60e97b1820a11824a86f3
Adrian Cole <acole@pivotal.io>
2018-08-19 17:57:28 +0800

    Bumps to latest Brave
    Brave no longer allows claiming duplicate IPs for a remote destination,
    specifically both ipv4 and ipv6. This is not intuitive anyway. This
    change prefers ipv6 when a remote connection is tagged with two IP
    addresses.

--- src/test/java/brave/opentracing/BraveTracerTest.java
+++ src/test/java/brave/opentracing/BraveTracerTest.java
@@ -74,8 +74,7 @@ public class BraveTracerTest {
         .tag("lc", "codec")
         .start(1L);
 
-    io.opentracing.Span openTracingSpan =
-        new BraveSpan(brave.tracer(), braveSpan, BraveSpan.EMPTY_ENDPOINT);
+    io.opentracing.Span openTracingSpan = new BraveSpan(brave.tracer(), braveSpan);
 
     openTracingSpan.log(2L, "pump fake");
     openTracingSpan.finish(3L);
